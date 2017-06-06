605e306e8bdfc516d18635d13078c8bb5d9122ea
Dylan McCurry <dylan@brainjocks.com>
2017-06-06 09:17:39 -0400

    Adding a prototype of the Page Data component

---
+++ sc821.Custom/RenderingParameters/PageContentRenderingParameters.cs
@@ -0,0 +1,25 @@
+﻿using Score.Data.Extensions;
+using Score.UI.Data.RenderingParameters;
+using Sitecore.Mvc.Presentation;
+
+namespace sc821.Custom.RenderingParameters
+{
+    public class PageContentRenderingParameters : BaseComponentParameters
+    {
+        public static class Fields
+        {
+            public static string FieldName = "Field Name";
+            public static string Markup = "Markup";
+        }
+
+        public string FieldName { get; set; }
+        public string Markup { get; set; }
+
+        public override void LoadRenderingParameters(Rendering rendering)
+        {
+            base.LoadRenderingParameters(rendering);
+            this.FieldName = rendering.Parameters.GetUserFriendlyValue(Fields.FieldName);
+            this.Markup = rendering.Parameters.GetUserFriendlyValue(Fields.Markup);
+        }
+    }
+}
---
+++ sc821.Web/Areas/sc821/Models/PageContentModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using sc821.Custom.RenderingParameters;
+using Score.UI.Web.Areas.ScoreUI.Models;
+using Sitecore.Mvc.Presentation;
+
+namespace sc821.Web.Areas.sc821.Models
+{
+    public class PageContentModel : RenderingModelBase<PageContentRenderingParameters>
+    {
+        public PageContentModel() : base("page-content") { }
+
+        public string Markup { get; set; }
+
+        public string Field { get; set; }
+
+        public bool Initialized
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(this.Markup) && !string.IsNullOrWhiteSpace(this.Field);
+            }
+        }
+
+        public override void Initialize(Rendering rendering)
+        {
+            base.Initialize(rendering);
+            this.Markup = this.RenderingParameters.Markup;
+            this.Field = this.RenderingParameters.FieldName;
+        }
+    }
+}
